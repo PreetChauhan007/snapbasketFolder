b0d219d78f9d422487c30a7f01ea4b84e09f8bd2
PreetChauhan <preetchauhan842@gmail.com>
2026-08-13 16:18:11 +0530

    footer change

--- snapbasket/src/components/Footer.tsx
+++ snapbasket/src/components/Footer.tsx
@@ -34,7 +34,7 @@ function Footer() {
     <h3 className='text-xl font-semibold mb-3'>Contact Us</h3>
     <ul className='space-y-2 text-purple-100 text-sm'>
         <li className='flex items-center gap-2'>
-            <MapPin size={16}/>Jaipur,Rajasthan,India
+            <MapPin size={16}/>Noida,Uttar Pradesh,India
         </li>
         <li className='flex items-center gap-2'>
             <Phone size={16}/> +91 9876543210
